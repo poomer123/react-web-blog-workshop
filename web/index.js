6dc3c0d0139a8b8bf9484c4bd590af1e12680361
Apirat Chiwakanit <apirat.chiwa@gmail.com>
2018-01-04 18:34:42 +0700

    เข้าใจ Redux แบบพื้นฐาน

--- web/index.js
+++ web/index.js
@@ -3,13 +3,39 @@ import './index.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {match, Router, browserHistory} from 'react-router'
+import {createStore} from 'redux'
 import routes from './routes'
+import {Provider} from 'react-redux'
+
+
+// (state, action) => new state
+function counter( state = 0, action ) {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1
+		case 'DECREMENT':
+			return state - 1
+		default:
+			return state
+	}
+}
+
+const store = createStore(counter)
+store.subscribe(() => {
+	console.log(store.getState())
+})
+console.log( store.getState() )
+store.dispatch( {type: 'INCREMENT'} )
+
 
 match(
 	{history: browserHistory, routes},
 	(error, redirectLocation, renderProps) => {
 		ReactDOM.render(
-			<Router {...renderProps} />,
+			<Provider store={store}>
+				<Router {...renderProps} />
+			</Provider>,
+
 			document.getElementById('app')
 		)
 	}
