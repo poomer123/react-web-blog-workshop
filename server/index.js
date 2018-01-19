72d2c0e561f4e6e5eb9f0408e0b7904ec8bc6818
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-01-19 17:00:19 +0700

    GraphQL - แนวคิดใหม่ที่ฮอตฮิตที่สุด (part 1)

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const favicon = require('serve-favicon')
 const compression = require('compression')
+const proxy = require('http-proxy-middleware')
 const renderer = require('./renderer')
 const paths = require('../config/paths')
 const app = express()
@@ -11,6 +12,10 @@ if( process.env.NODE_ENV === 'production' ) {
 }
 app.use(favicon(path.resolve(paths.public, 'favicon.ico')))
 app.use(express.static(paths.public))
+app.use('/api', proxy({
+	target: 'http;//localhost:3000',
+	changeOrigin : true,
+}))
 app.use(renderer)
 app.listen(8000, () => {
 	console.log('Server Ready 8000')
